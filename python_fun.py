0668bdf005575566ca9d7f95976434af33c9853f
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-04 23:08:39 +0200

    some new examples

--- python_fun.py
+++ python_fun.py
@@ -1,3 +1,30 @@
-
+#adding phrases
 phrase = "going to"
-print ( phrase + " clean code!")
+print ( phrase + " clean code!".upper())
+
+print ("")
+
+#letter counting
+phrase = "going to"
+print (len(phrase + " clean code!".upper()))
+
+print("")
+
+#printing in new lines (\n)
+phrase = "going to"
+print ( phrase + " clean \n code!".upper())
+
+print ("")
+
+#show letter where index
+phrase = "going to"
+sentence = ( phrase + " clean code!")
+print (sentence[0])
+
+print ("")
+
+#show index where letter or letters
+phrase = "going to"
+sentence = ( phrase + " clean code!")
+print (sentence.index("i"))
+
